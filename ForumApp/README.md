bf8251fdadd8c15f4bfe382f402d6b7c9d87eb36
skipjac <skip@skipjack.info>
2012-10-12 10:58:54 -0700

    Adding new app that provides shortcuts and quick search tool for your forum. This is a widget in Classic

---
+++ ForumApp/README.md
@@ -0,0 +1,5 @@
+Zendesk Forum App
+==========
+
+This is rebuild of an old Zendesk widget provided shortcuts and quick search tool for your forum. You can click on the link to open a new window/tab to that forum or search the forum. 
+
---
+++ ForumApp/app.js
@@ -0,0 +1,66 @@
+(function(){
+ 
+  return {
+    appID:  'FourmApp',
+    defaultState: 'loading',
+    forums: [],
+    events: {
+      'app.activated': 'onLoad',
+      'getForumID.done': 'processForums',
+      'blur input#searchText': 'searchString'
+    }, //end events
+    requests: {
+        getForumID: function() {
+          return {
+            url: '/api/v2/forums.json',
+            dataType: 'JSON',
+            type: 'GET',
+            contentType: 'application/json'
+            };
+          },
+          getTopicCount: function (id) {
+            return {
+            url: '/api/v2/forums/' + id + '/topics.json',
+            dataType: 'JSON',
+            type: 'GET',
+            contentType: 'application/json'
+          };
+        }
+      },//end requests
+
+    onLoad: function(){
+      if (this.forums.length > 0 ){
+        this.displayLinks();
+      } else {
+         this.ajax('getForumID');
+      }
+     
+    },
+    processForums: function(data) {
+      var forumList = data.forums;
+      this.forums = data.forums;
+      this.forums.forEach(function (x) {
+        this.ajax('getTopicCount', x.id).done(function(data) {
+          x.count = data.count;
+          this.displayLinks();
+        }, this);
+      }, this);
+    },
+    displayLinks: function () {
+      this.switchTo('links', { 
+        createResult: this.forums
+      });
+    },
+    searchString: function () {
+      var query = this.$('#searchText').val();
+      console.log(query);
+      this.forums.forEach(function (x) {
+        x.queryString = query;
+      });
+      this.switchTo('links', { 
+        createResult: this.forums,
+        queryString: query
+      });
+    }
+  };
+}());
