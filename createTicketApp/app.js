5ca03dbc0b0e3487def12131fc0c1c11a7af112d
skipjac <skip@skipjack.info>
2012-11-13 16:14:18 -0800

    Adding create ticket in spoke to public zendesk

---
+++ createTicketApp/app.js
@@ -0,0 +1,51 @@
+(function(){
+  return {
+	  appID: 'https://github.com/zendesk/widgets/tree/master/createTicketApp',
+	  defaultState: 'layout',
+	  name: '',
+	  groups: [],
+	  targetSpoke: this.$('#zendeskSelect').val() || '',
+	  newRequester: this.$('#userName').val() || '',
+	  newEmail: this.$('#userEmail').val() || '',
+	  newSub: this.$('#userSub').val() || '',
+	  newDesc: this.$('#ticketDesc').val() || '',
+	
+	events: {
+		'click .submitSpoke': 'createTicketValues',
+		'createTicket.done': 'processData',
+		'click .displayForm': 'switchToReqester'
+	}, //end events
+	requests: {
+			createTicket: function() {
+			  return {
+			    url: 'http://' + this.targetSpoke + '/requests/embedded/create/?subject=' + this.newSub + '&description='+ this.newDesc + '&name=' + this.newRequester  + '&email='+ encodeURI(this.newEmail) + '',
+				dataType: 'JSON'
+			  };
+			}	
+	    }, //end requests 
+	processData: function(data, response, responseText) {
+		console.log(responseText);
+		
+		this.switchTo('description', { 
+			newTicket: data.message,
+			spoke: this.targetSpoke
+		});
+		
+	},
+	createTicketValues: function() {
+		this.targetSpoke = this.$('#zendeskSelect').val();
+  	    this.newRequester = this.$('#userName').val();
+  	    this.newEmail = this.$('#userEmail').val();
+  	    this.newSub = this.$('#userSub').val();
+  	    this.newDesc = this.$('#ticketDesc').val();
+		this.ajax('createTicket');
+		},
+	switchToReqester: function(data) {
+		this.switchTo('requester', { 
+		});
+		
+	}
+	
+  } //end first return
+}());
+
