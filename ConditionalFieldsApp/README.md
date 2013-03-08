89d67cec55ae74cd2bef5df6da0744d5960cdc45
skipjac <skip@skipjack.info>
2013-03-08 12:28:45 -0800

    updating condition fields to use the settings page

--- ConditionalFieldsApp/README.md
+++ ConditionalFieldsApp/README.md
@@ -1,33 +1,26 @@
 Conditional Fields
 ==========
 
-This New Zendesk app will show and hide fields based on the value of a dropdown. 
+This New Zendesk app will show and hide fields based on the value of a dropdown. This app requires some basic knowledge of javascipt and JSON to configure. 
 
-Because of the customizable nature required from the code there isn't a way of handling the configuration from the settings page, so all configuration is done in the code. Conditional fields are designed to simplify workflow. Take some time and think about the fields you need and when they should be displayed. The basic working block of conditional fields is the array. You will build arrays of ticket field id's you which to effect when the option is selected from a monitor field.
- 
-```javascript
-var projectRequest = ['280865','223223'];
-var otherHelpesk = ['279466'];
+The configuration is a two step process where you have to edit the app.js file to configure your watched fields in the events object. Also adding varibles in capture the value of the watched fields. Watched fields are the fields that when a option is selected other fields will either be hidden or become visible. 
+
+In app.js you will see a javascript object called events:
+
+``` javascript
+    events: {
+      'app.activated':  'init',
+      'requiredProperties.ready': 'toggleFields',
+      'ticket.custom\_field\_21631456.changed': 'toggleFields'
+    }
 ```
 
-The changes to the monitor fields have event handlers which can fire functions ```.changed``` . ```app.activated``` is a special hander that fires when the app first loads or a new tab in the app is opened. 
+The key ``` 'ticket.custom\_field\_21631456.changed' ``` is a watched field. For each watched field you need to add a new key and action. ``` 'toggleFields' ``` is the action to take when field 21631456 is changed, in this case if fires a function. In the function we get the value of 21631456 and using the JSON that was set in the settings page we get the array of fields to act upon. 
 
-```javascript
-events: {
-  'app.activated': 'setValue',
-  'ticket.custom_field_21631456.changed': 'typeII'
-},
+The JSON object needs to look like: 
+
+``` javascript
+{"cat":[21875871,21865183,21745801], "dog":[22103126,21613267], "dolphin": [280865, 20295661], "the_fish": []}
 ```
 
-The function that controls what happens when a montior option is selected uses to helper functions ```show(fields)``` and ```hide(fields)``` where *fields* is an array of ticket field id's which is returned from a hash map. 
-
-```javascript
-typeII: function(){
-  this.hide(hideAll);
-  this.type2thing = this.ticket().customField('custom_field_21631456');
-  //console.log('control field value: ' + this.type2thing);
-  if (this.type2thing != null) {
-   this.show(moodFieldMap[this.type2thing]);
-  }
-},
-```
+Each key is a option value in the drop down field that is being watched, in this case 21631456. Each key needs an array of ticket field ID's to show when that value is selected. Please read up on the formating of JSON objects at [Introducing JSON](http://json.org/) . 
--- ConditionalFieldsApp/app.js
+++ ConditionalFieldsApp/app.js
@@ -1,43 +1,80 @@
 (function(){
-  var fieldGroupOne = [ 'custom_field_280865' ],
-      fieldGroupTwo = [ 'custom_field_279466' ],
-
-      allFieldsToToggle = [].concat( fieldGroupOne, fieldGroupTwo ),
-
-      fieldMap = {
-        cat:     [].concat( fieldGroupOne ),
-        dog:     [].concat( fieldGroupTwo ),
-        dolphin: [].concat( fieldGroupOne, fieldGroupTwo )
-      };
-
   return {
+    currAttempt : 0,
+    MAX_ATTEMPTS : 20,
     appID:  'https://github.com/zendesk/widgets/tree/master/ConditionalFieldsApp',
-
-    defaultState: 'loading',
-
+    requiredProperties: [],
+    allFieldsToToggle: [],
+    fieldMap: {},
     events: {
-      'app.activated':                        'toggleFields',
+      'app.activated':  'init',
+      'requiredProperties.ready': 'toggleFields',
       'ticket.custom_field_21631456.changed': 'toggleFields'
     },
-
-    toggleFields: function(){
-      this.hide(allFieldsToToggle);
-      var conditionValue = this.ticket().customField('custom_field_21631456');
-      if ( conditionValue == null ) { return; }
-      var fieldsToShow = allFieldsToToggle[ conditionValue ];
-      if (fieldsToShow == null) { return; }
-      this.show(fieldsToShow);
+    init: function(){
+      //convert the string from the settings in to a javascript object
+      this.fieldMap = JSON.parse(this.settings.fieldMap);
+      Object.keys(this.fieldMap).forEach(function(key){
+        //get the all the field values from the settings object and put into arrays
+        this.requiredProperties = this.requiredProperties.concat(this.fieldMap[key]);
+        this.allFieldsToToggle = this.allFieldsToToggle.concat(this.fieldMap[key]);
+      }, this);
+      //call allRequiredPropertiesExist to ensure all the field values in the object exist
+      this.allRequiredPropertiesExist();
     },
-
+    toggleFields: function(){
+      this.hide(this.allFieldsToToggle);
+      var thereAreNulls = [undefined, null, '', '-'];
+      var conditionValue = this.ticket().customField('custom_field_21631456');
+      if (_.indexOf(thereAreNulls, conditionValue) === -1) {
+        this.show(this.fieldMap[conditionValue]);
+      } 
+    },
+    allRequiredPropertiesExist: function() {
+      if (this.requiredProperties.length > 0) {
+        var valid = this.validateRequiredProperty(this.requiredProperties[0]);
+        // prop is valid, remove from array
+        if (valid) {
+          this.requiredProperties.shift();
+        }
+        if (this.requiredProperties.length > 0) {
+          if (!valid) {
+            ++this.currAttempt;
+          }
+          _.delay(_.bind(this.allRequiredPropertiesExist, this), 100);
+          return;
+        }
+      }
+      if (this.currAttempt < this.MAX_ATTEMPTS) {
+        this.trigger('requiredProperties.ready');
+      } else {
+        this.services.notify(this.I18n.t('global.error.data'));
+      }
+    },
+    validateRequiredProperty: function(property) {
+      var parts = 'custom_field_' + property;
+      var part = '', obj = this;
+          obj = this.ticketFields(parts);
+        // check if property is invalid
+        if (!_.isObject(obj)) {
+          return false;
+        }
+        // check if value returned from property is invalid
+        if (parts.length === 0 && (_.isNull(obj) || _.isUndefined(obj) || obj === '' || obj === 'no')) {
+          return false;
+        }
+      //}
+      return true;
+    },
     hide: function(fields){
       fields.forEach(function(field) {
-        this.ticketFields(field).hide();
+        this.ticketFields('custom_field_' + field).hide();
       }, this);
     },
 
     show: function(fields) {
       fields.forEach(function(field) {
-        this.ticketFields(field).show();
+        this.ticketFields('custom_field_' + field).show();
       }, this);
     }
   };
