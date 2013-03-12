333f0b78d0de3bae1c40a3feb83de1e118f2bb42
skipjac <skip@skipjack.info>
2013-03-12 15:42:11 -0700

    this tabs are fixed now

--- fivemostrecenttickets/app.js
+++ fivemostrecenttickets/app.js
@@ -1,61 +1,60 @@
 (function() {
-	return {
-    //
-		currAttempt : 0,
-		MAX_ATTEMPTS : 20,
-		events: {
-			'app.activated':'init',
-			'fullUserData.done': 'handleUserResults',
-			'requiredProperties.ready': 'getUserData'
-		},
-		requiredProperties : [
-			'ticket.requester.email'
-		],
-		requests: {
-			fullUserData: function(userID) {
-				return {
-					url: '/api/v2/users/' + userID + '/tickets/requested.json?sort_order=desc',
-					dataType: 'json'
-				};
-      }
-		},
-		init: function(){
-			this.allRequiredPropertiesExist();
-		},
-		getUserData: function() {
-			this.ajax( 'fullUserData', this.ticket().requester().id() );
-		},
-		handleUserResults: function(data) {
-			var lastestFive = _.first(data.tickets, 5).sort(function(a,b) { 
-				var aID = a.id;
-				var bID = b.id;
-				return (aID === bID) ? 0 : (aID < bID) ? 1 : -1;
-			});
-			this.switchTo('lastfive', {
-				lastestFiveArr: lastestFive
-			});
-		},
-		allRequiredPropertiesExist: function() {
-			if (this.requiredProperties.length > 0) {
-				var valid = this.validateRequiredProperty(this.requiredProperties[0]);
-				// prop is valid, remove from array
-				if (valid) {
-					this.requiredProperties.shift();
-				}
-				if (this.requiredProperties.length > 0 && this.currAttempt < this.MAX_ATTEMPTS) {
-					if (!valid) {
-						++this.currAttempt;
-					}
-					_.delay(_.bind(this.allRequiredPropertiesExist, this), 100);
-					return;
-				}
-			}
-			if (this.currAttempt < this.MAX_ATTEMPTS) {
-				this.trigger('requiredProperties.ready');
-			} else {
-				services.notify(this.I18n.t('global.error.data'), 'error');
-			}
-		},
+    return {
+    currAttempt : 0,
+    MAX_ATTEMPTS : 20,
+    events: {
+        'app.activated':'init',
+        'fullUserData.done': 'handleUserResults',
+        'requiredProperties.ready': 'getUserData'
+    },
+    requiredProperties : [
+        'ticket.requester.email'
+    ],
+    requests: {
+        fullUserData: function(userID) {
+          return {
+              url: '/api/v2/users/' + userID + '/tickets/requested.json?sort_order=desc',
+              dataType: 'json'
+          };
+        }
+    },
+    init: function(){
+      this.allRequiredPropertiesExist();
+    },
+    getUserData: function() {
+      this.ajax( 'fullUserData', this.ticket().requester().id() );
+    },
+    handleUserResults: function(data) {
+      var lastestFive = _.first(data.tickets, 5).sort(function(a,b) { 
+        var aID = a.id;
+        var bID = b.id;
+        return (aID === bID) ? 0 : (aID < bID) ? 1 : -1;
+      });
+      this.switchTo('lastfive', {
+        lastestFiveArr: lastestFive
+      });
+    },
+    allRequiredPropertiesExist: function() {
+      if (this.requiredProperties.length > 0) {
+        var valid = this.validateRequiredProperty(this.requiredProperties[0]);
+        // prop is valid, remove from array
+        if (valid) {
+            this.requiredProperties.shift();
+            }
+        if (this.requiredProperties.length > 0 && this.currAttempt < this.MAX_ATTEMPTS) {
+          if (!valid) {
+             ++this.currAttempt;
+          }
+          _.delay(_.bind(this.allRequiredPropertiesExist, this), 100);
+              return;
+          }
+        }
+        if (this.currAttempt < this.MAX_ATTEMPTS) {
+                this.trigger('requiredProperties.ready');
+        } else {
+          services.notify(this.I18n.t('global.error.data'), 'error');
+        }
+    },
     objectAt: function(path) {
       return _.inject( path.split('.'), function(self, segment) {
         console.log(self);
@@ -66,5 +65,5 @@
     validateRequiredProperty: function(propertyPath) {
       return this.objectAt(propertyPath) != null;
     }
-	};
+  };
 }());
