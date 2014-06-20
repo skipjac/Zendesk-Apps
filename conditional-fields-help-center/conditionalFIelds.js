62d5ceda1f2f85ec4cdd9d379cfefa20b5e20a8d
skipjac <skip@skipjack.info>
2014-06-20 10:29:16 -0700

    updating Required fields app

--- conditional-fields-help-center/conditionalFIelds.js
+++ conditional-fields-help-center/conditionalFIelds.js
@@ -1,4 +1,7 @@
-  (function () {
+// ************************* Conditional Fields ***********************************
+ $(document).ready(function() {
+
+  //console.log('in cond fields');
     //use the same object is used in the conditional fields app for the new Zendesk agent interface
     var setting = '{"custom_field_21631456": {"cat":["custom_field_21875871","custom_field_21865183","custom_field_21745801"], "dog":["custom_field_22103126","custom_field_21613267"], "dolphin": ["custom_field_280865", "custom_field_20295661"], "the_fish": ["custom_field_279466"]}, "custom_field_279466":{"asdf":["custom_field_21651413"], "xfer_to_a": [], "fire_to_new_page": []}}';
     var settingMap = JSON.parse(setting);
@@ -6,99 +9,123 @@
     var requiredProperties = [];
     var fieldMap = [];
     var newArray = [];
-    var allFieldsToToggle =[];
+    var allFieldsToToggle = [];
     //this gets the fields that are on the current request page 
-    var availFields = $('.form-field [name]').map(function(){
+    var availFields = $('.form-field [name]').map(function() {
       return this.name;
     });
     //show fields
     var show = function() {
-      $.each(arguments, function(i, item) {
-        _.each(item, function(i){$('[name="' + i +'"]').parents('.form-field').css('display', '')});
-      });
-    };
+        $.each(arguments, function(i, item) {
+          _.each(item, function(i) {
+            $('[name="' + i + '"]').parents('.form-field').css('display', '').addClass('required').removeClass('optional');
+          });
+        });
+      };
     //hide fields
     var hide = function(hide, show) {
-      hide.forEach(function(field){
-         if (!_.contains(show, field)) {
-          clearValue(field);
-          $('[name="' + field +'"]').parents('.form-field').css('display', 'none');
-         }
-       }, this);
-    };
+        hide.forEach(function(field) {
+          if (!_.contains(show, field)) {
+            clearValue(field);
+            $('[name="' + field + '"]').parents('.form-field').css('display', 'none').removeClass('required');
+          }
+        }, this);
+      };
     //clear the fields that are hidden
-    var clearValue = function(field){
-      $('input[name="' + field +'"]').filter('[value=]').prop('checked', true);
-      if($('[name="' + field +'"]').parent().hasClass('text')){
-        console.log('this is a text field', field);
-        $('[name="' + field +'"]').val('');
-      }
-      $('[name="' + field +'"]').parents('.nesty').children('.nesty-input').map(function(x,y){$(y).text('');});
-    };
-    //convert the settingMap to match the Help Center DOM
-    var convertField = function(field){
-      var newName;
-      if(_.isObject(field)){
-        newName = [];
-        field.forEach(function(x){
-          var e = x.split('_');
-          newName.push('request[custom_fields]['+ e[e.length -1] +']');
+    var clearValue = function(field) {
+        $('input[name="' + field + '"]').filter('[value=]').prop('checked', true);
+        if ($('[name="' + field + '"]').parent().hasClass('text')) {
+          
+          $('[name="' + field + '"]').val('');
+        }
+        $('[name="' + field + '"]').parents('.nesty').children('.nesty-input').map(function(x, y) {
+          $(y).text('');
         });
-      } else if (_.isString(field)){
-        var e = field.split('_');
-        newName = 'request[custom_fields]['+ e[e.length -1] +']';
-      }
-      return newName;
-    };
+      };
+    //convert the settingMap to match the Help Center DOM
+    var convertField = function(field) {
+        var newName;
+        if (_.isObject(field)) {
+          newName = [];
+          field.forEach(function(x) {
+            var e = x.split('_');
+            newName.push('request[custom_fields][' + e[e.length - 1] + ']');
+          });
+        } else if (_.isString(field)) {
+          var e = field.split('_');
+          newName = 'request[custom_fields][' + e[e.length - 1] + ']';
+        }
+        return newName;
+      };
     //build the all fields toggle for the current page. 
-    var setFields = function(){
-      Object.keys(settingMap).forEach(function(key){
-            watchFields.push(convertField(key));
+    var setFields = function() {
+        Object.keys(settingMap).forEach(function(key) {
+          watchFields.push(convertField(key));
+
           //get the all the field values from the settings object and put into arrays
           Object.keys(settingMap[key]).forEach(
-            function(innerKey){
-              fieldMap[innerKey] = convertField(settingMap[key][innerKey]);          
-              requiredProperties = requiredProperties.concat(fieldMap[innerKey]);
-            }, this);
+
+          function(innerKey) {
+            fieldMap[innerKey] = convertField(settingMap[key][innerKey]);
+            //fieldMap[innerKey] = settingMap[key][innerKey]
+            requiredProperties = requiredProperties.concat(fieldMap[innerKey]);
+          }, this);
         }, this);
         allFieldsToToggle = _.intersection(availFields, requiredProperties);
-        toggleFields(allFieldsToToggle);     
-    };
+        toggleFields(allFieldsToToggle);
+      };
     //call the show and hide based on the watched fields selected values
-    var toggleFields = function(allFieldsToToggle){
-      var thereAreNulls = [undefined, null, '', '-'];
-      var count = 0;
-      var fieldsTo = [];
-      var fieldsToShow = [];
-      //build the show fields object
-      watchFields.forEach(function(watchedID){
-        var selectionval = $('input[name="' + watchedID +'"]:checked').val();
-        count = count + 1;
-        if (_.indexOf(thereAreNulls, selectionval) === -1) {
-          var fields = _.intersection(availFields, fieldMap[selectionval]);
-          fieldsToShow = _.union(fieldsToShow, fields);
-        }
-        if( count === watchFields.length ) {
-          var somethingUsful = _.intersection(allFieldsToToggle, watchFields);
-          //remove any fields that are that are in a hidden watched field
-          somethingUsful.forEach(function(x){
-            if(!_.contains(fieldsToShow, x)){
-              var y = $('input[name="' + x +'"]:checked').val();
-              fieldsToShow = _.difference(fieldsToShow, fieldMap[y]);
-            }
-          });
-          show(fieldsToShow);
-          hide(allFieldsToToggle, fieldsToShow);
-        }
-      }, this);
-    };
+    var toggleFields = function(allFieldsToToggle) {
+        var thereAreNulls = [undefined, null, '', '-'];
+        var count = 0;
+        var fieldsTo = [];
+        var fieldsToShow = [];
+        //build the show fields object
+
+        watchFields.forEach(function(watchedID) {
+
+          var selectionval = $('input[name="' + watchedID + '"]').val();
+          //console.log("selectionval ", selectionval);
+          //var selectionval = $('input[name="' + watchedID + '"]:checked').val();
+
+          count = count + 1;
+          if (_.indexOf(thereAreNulls, selectionval) === -1) {
+            var fields = _.intersection(availFields, fieldMap[selectionval]);
+            fieldsToShow = _.union(fieldsToShow, fields);
+            //console.log("fields ", fields, "fieldsToShow", fieldsToShow );
+
+          }
+          if (count === watchFields.length) {
+
+            var somethingUsful = _.intersection(allFieldsToToggle, watchFields);
+            //console.log("somethingUsful ", somethingUsful);
+            //remove any fields that are that are in a hidden watched field
+            somethingUsful.forEach(function(x) {
+              if (!_.contains(fieldsToShow, x)) {
+                //var y = $('input[name="' + x + '"]:checked').val();
+                var y = $('input[name="' + x + '"]').val();
+                //console.log("y ", y);
+                fieldsToShow = _.difference(fieldsToShow, fieldMap[y]);
+              }
+            });
+            show(fieldsToShow);
+            hide(allFieldsToToggle, fieldsToShow);
+          }
+        }, this);
+      };
     //call set fields on page load
     setFields();
     // watch the fields for changes
-    $('.form-field').change(function(e){
-      if(_.contains(watchFields, e.target.name)){
+    $('input').change(function(e){
+      //console.log("in change", watchFields, e.target.name, allFieldsToToggle);
+      if (_.contains(watchFields, e.target.name)) {
+        //console.log("true");
         toggleFields(allFieldsToToggle);
       }
-           
     });
-  }());
+  });
+  
+  //********************************  END Conditional Fields  ***********************************
+
+
+  
