3fa96a299af67a115d5c5e75ec3bd76bcba99670
skipjac <skip@skipjack.info>
2014-06-20 10:25:24 -0700

    updating Required fields app

--- RequiredFieldsApp/README.md
+++ RequiredFieldsApp/README.md
@@ -1,35 +1,14 @@
-Required Fields App
-==========
+# App name
 
-This New Zendesk app fill force agents in your Zendesk to fill out required fields before they can submit a update to a ticket. They can be any field you want with in the system, you just have to define them in the code. 
+[brief description of the app]
 
-```javascript
-var firstField = this.getFieldValue(this.ticket().customField('custom_field_21631456'));
-var secondField = this.getFieldValue(this.ticket().customField('custom_field_21613267'));
-var thirdField = this.getFieldValue(this.ticket().customField('custom_field_280865'));
-var checkedFields = [firstField, secondField, thirdField];
-```
+### The following information is displayed:
 
-Then just set up events to monitor changes in those fields. 
+* info1
+* info2
+* info3
 
-```javascript
-events: {
-  'app.activated': 'setValue',
-  'ticket.custom_field_21631456.changed': 'typeII',
-  'ticket.custom_field_21613267.changed': 'typeII',
-  'ticket.custom_field_280865.changed': 'typeII'
-}, //end events
-```
+Please submit bug reports to [Insert Link](). Pull requests are welcome.
 
-The ```getFieldValue``` function looks at the returned value from the ticket API call and see if it is returned ```undefined, null or empty``` this is becuase in Zendesk if the ticket field did exist when th ticket was created it's not saved to the ticket in the database. 
-
-```javascript
-getFieldValue: function (fieldValue) { 
-   var thereAreNulls = [undefined, null, '', 'no'];
-    if(_.indexOf(thereAreNulls, fieldValue) === -1) {
-      return fieldValue;
-    } else {
-      return '';
-    }
-  }
-  ```
+### Screenshot(s):
+[put your screenshots down here.]
--- RequiredFieldsApp/app.js
+++ RequiredFieldsApp/app.js
@@ -1,41 +1,60 @@
-(function(){
-
-  function fieldValueIsBlank(value) {
-    // "no" is for checkbox fields
-    return value == null || value === '' || value === 'no';
-  }
-
-  function anyValueIsBlank = function(values) {
-    return _.any(values, fieldValueIsBlank);
-  }
+(function() {
 
   return {
-    appID:  'https://github.com/skipjac/Zendesk-Apps/tree/master/RequiredFieldsApp',
-    defaultState: 'loading',
-
+    formFields: {},
     events: {
-      'app.activated': 'toggleSave',
-      'ticket.custom_field_21631456.changed': 'toggleSave',
-      'ticket.custom_field_21613267.changed': 'toggleSave',
-      'ticket.custom_field_280865.changed': 'toggleSave'
-    }, //end events
-
-    checkedFieldValues: function() {
-      return [
-        this.ticket().customField('custom_field_21631456'),
-        this.ticket().customField('custom_field_21613267'),
-        this.ticket().customField('custom_field_280865')
-      ];
+      'app.activated':'getSettings',
+      'getTicketForms.done': 'buildFormArr',
+      'ticket.save': 'checkValue'
     },
-
-    toggleSave: function(){
-      if ( anyValueIsBlank(this.checkedFieldValues()) ) {
-        services.notify(this.I18n.t('fields.more'), 'error');
-        this.disableSave();
-      } else {
-        this.enableSave();
+    requests:{
+      getTicketForms: function() {
+        return {
+          url: '/api/v2/ticket_forms.json',
+          dataType: 'JSON',
+          type: 'GET',
+          proxy_v2: true
+        };
       }
+    },
+    getSettings: function() {
+      var stringOfFields = this.setting('fieldIDs');
+      this.arrayOfields = stringOfFields.split(',');
+      this.arrayOfields.forEach(function(x, key){
+        this.arrayOfields[key] = parseInt(x, 10);
+      }, this);
+      this.ajax('getTicketForms');
+    },
+    concatFieldsSting: function(arr){
+      var arrayFields = arr;
+      arrayFields.forEach(function(x, key){
+        arrayFields[key] = 'custom_field_' + x;
+      }, this);
+      return arrayFields;
+    },
+    buildFormArr: function(data){
+      data.ticket_forms.forEach(function(x){
+        this.formFields[x.id] = x.ticket_field_ids;
+      }, this);
+    },
+    checkValue: function(){
+      var valid = true;
+      var badFieldsstring = '';
+      var fieldsInForm = _.intersection(this.arrayOfields, this.formFields[this.ticket().form().id()]);
+      if (fieldsInForm.length === 0 ) return true;
+      var fixedArray = this.concatFieldsSting(fieldsInForm);
+      fixedArray.forEach(function(x){
+        var thereAreNulls = [undefined, null, '', '-'];
+        if(_.indexOf(thereAreNulls, this.ticket().customField(x)) !== -1){
+          valid = false;
+          badFieldsstring += this.ticketFields(x).label() + '</br>';
+        }
+      }, this);
+      if(!valid){
+        return "Unable to submit ticket because the following fields are missing values </br>" + badFieldsstring;
+      }
+      return true;
     }
-  }; //end of first return
+  };
 
 }());
